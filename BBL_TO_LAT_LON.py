37f278c80f699799860513bb7d89ef73fa2847cc
AlaaMoussawi <alaamoussawi@gmail.com>
2018-12-07 17:58:01 -0500

    added try except functionality

--- BBL_TO_LAT_LON.py
+++ BBL_TO_LAT_LON.py
@@ -6,16 +6,19 @@ import os
 # os.environ['NYC_GEOCLIENT_KEY']
 
 def get_lat_lon_from_bin(bin_number):
-    
-    base_url = 'https://api.cityofnewyork.us/geoclient/v1/bin.json'
-    api_id = os.environ['NYC_GEOCLIENT_ID']
-    api_key = os.environ['NYC_GEOCLIENT_KEY']
-    
-    PARAMS = {'bin':bin_number, 'app_id':api_id, 'app_key':api_key}
-    output = (requests.get(url = base_url, params = PARAMS)).json()
-    
-    return (output['bin']['latitudeInternalLabel'], output['bin']['longitudeInternalLabel'])
 
+    try:
+        base_url = 'https://api.cityofnewyork.us/geoclient/v1/bin.json'
+        api_id = os.environ['NYC_GEOCLIENT_ID']
+        api_key = os.environ['NYC_GEOCLIENT_KEY']
+        
+        PARAMS = {'bin':bin_number, 'app_id':api_id, 'app_key':api_key}
+        output = (requests.get(url = base_url, params = PARAMS)).json()
+    
+        return (output['bin']['latitudeInternalLabel'], output['bin']['longitudeInternalLabel'])
+    except:
+        return(0, 0)
+    
 def get_lat_lon_from_bin_array(bin_numbers):
     
     base_url = 'https://api.cityofnewyork.us/geoclient/v1/bin.json'
@@ -24,8 +27,12 @@ def get_lat_lon_from_bin_array(bin_numbers):
     result = []
     
     for bin_number in bin_numbers:
-        PARAMS = {'bin':bin_number, 'app_id':api_id, 'app_key':api_key}
-        output = (requests.get(url = base_url, params = PARAMS)).json()
-        result.append( (output['bin']['latitudeInternalLabel'], output['bin']['longitudeInternalLabel']) )
+        try:
+            PARAMS = {'bin':bin_number, 'app_id':api_id, 'app_key':api_key}
+            output = (requests.get(url = base_url, params = PARAMS)).json()
+            result.append( (output['bin']['latitudeInternalLabel'], output['bin']['longitudeInternalLabel']) )
 
+        except:
+            result.append( (0, 0 )
+            
     return result
